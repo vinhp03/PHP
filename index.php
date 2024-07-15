e402d6224592716b36f931bb4072262627dfee06
vinhp03 <thevinhp333@gmail.com>
2024-07-15 09:35:06 +0700

    tes xong roi xoa

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<style>
+    
+</style>
+
+<head>
+    <title>Vi du the div trong html</title>    
+</head>
+
+<body>
+    <div id="menu" align="middle">
+        <a href="http://vnexpress.net">Trang tin tức</a>
+        <a href="http://cardina.vn">Sản phẩm - thời trang</a>
+        <a href="#">Giới thiệu</a>
+    </div id="content" align="left" bgcolor="red">
+        <h5>
+            Vi du the div trong html
+        </h5>
+         Chao cac ban den voi HTML 
+    <div>
+</body>
+</html>
